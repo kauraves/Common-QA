f0f7e42f839c5fd12c0a8126c647c08fd61d4ab8
Shelendra404 <niina.jarvinen@gmail.com>
2020-05-29 21:32:48 +0300

    Finish building authentication and communication between Firebase/Firestore and App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import SignIn from './components/sign-in/sign-in.component';
-
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -14,11 +13,31 @@ class App extends React.Component {
     };
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    // open messaging system between our App and firebase
+    // open subscription
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        // this will LISTEN to the userRef AND get back first state of that data
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
@@ -26,7 +45,8 @@ class App extends React.Component {
       <div className='App'>
         <h1>Hello</h1>
         <p>Our fancy groupwork comes here</p>
-        <SignIn name='Niina' />
+        <SignIn name={this.state.currentUser} />
+        <button onClick={() => auth.signOut()}>Sign out</button>
       </div>
     );
   }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
   apiKey: 'AIzaSyDwpoNIc-Bpbd-CXKfn_nqDIapHt9L3aE4',
@@ -11,9 +12,46 @@ const config = {
   appId: '1:322844869603:web:9ee6a84a30373c6e7416d6',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = userRef.get();
+  //this gives us also exists property that tells us whether the record exists in the database or not
+  //console.log(snapShot);
+
+  // if there is no data based on uid, create new user
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('Error creating user', error.message);
+    }
+  }
+  // now this has registered the user to firebase based on the uid
+  // userRef returns all the user object references
+  // i.e. userRef.id will return the id it is registered with
+  // This id could be used to pull userdata from mongoDB
+  //console.log(userRef);
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//console.log(firestore.collection('users').doc('gdjs25Yb8IaZAEk8XwIK'));
+
+//console.log(firestore.doc('users').doc('gdjs25Yb8IaZAEk8XwIK'));
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
